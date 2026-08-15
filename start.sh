0b013dd9b1043b49ad66c34858033cbd14f57c20
HzyEE <3492232377@qq.com>
2026-08-15 10:51:31 +0800

    Fix start.sh: replace wait -n with polling loop

--- start.sh
+++ start.sh
@@ -7,6 +7,11 @@ python gateway.py &
 GW_PID=$!
 
 trap "kill $BRAIN_PID $GW_PID 2>/dev/null; exit 1" TERM INT
-wait -n
+
+# Poll until either process exits
+while kill -0 $BRAIN_PID 2>/dev/null && kill -0 $GW_PID 2>/dev/null; do
+  sleep 5
+done
+
 kill $BRAIN_PID $GW_PID 2>/dev/null
 exit 1
